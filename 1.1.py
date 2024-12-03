5a7749af5c24d88ab568a38a334378dea716d0ad
Stefan Saraev <stefan.saraev@yahoo.com>
2024-12-03 11:58:24 +0200

    better day 1

--- 1.1.py
+++ 1.1.py
@@ -2,18 +2,15 @@ with open('1.txt', 'r') as f:
     data = f.read()
 
 numbers = [
-    list(filter(lambda x: x != '', line.strip().split(' ')))
+    list(line.strip().split('   '))
     for line in data.split('\n')]
 
-numbers = [[int(pair[0].strip()), int(pair[1].strip())] for pair in numbers]
+numbers = [[int(pair[0].strip()), int(pair[1].strip())]
+           for pair in numbers]
+
 left, right = zip(*numbers)
+left, right = sorted(left), sorted(right)
 
-left = sorted(left)
-right = sorted(right)
+S = sum(abs(l-r) for l, r in zip(left, right))
 
-S = 0
-
-for i in range(len(left)):
-    S += abs(left[i] - right[i])
-    
 print(S)
--- 1.2.py
+++ 1.2.py
@@ -1,28 +1,23 @@
+from collections import defaultdict
+
 with open('1.txt', 'r') as f:
     data = f.read()
 
 numbers = [
-    list(filter(lambda x: x != '', line.strip().split(' ')))
+    list(line.strip().split('   '))
     for line in data.split('\n')]
 
-numbers = [[int(pair[0].strip()), int(pair[1].strip())] for pair in numbers]
+numbers = [[int(pair[0].strip()), int(pair[1].strip())]
+           for pair in numbers]
+
 left, right = zip(*numbers)
 
-left = sorted(left)
-right = sorted(right)
-
-dct_freq = {}
+# default value 0
+dct_freq = defaultdict(int)
 
 for x in right:
-    if x not in dct_freq:
-        dct_freq[x] = 0
-    dct_freq[x] += 1
-    
-S = 0
-    
-for x in left:
-    if x not in dct_freq:
-        continue
-    S += x * dct_freq[x]
-    
+    dct_freq[x] = dct_freq[x] + 1
+
+S = sum(l * dct_freq[l] for l in left)
+
 print(S)
